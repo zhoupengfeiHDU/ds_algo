477ac11eee3cfb11da9346c35f3dd051a96998ba
zhoupengfei <zpf1996415@foxmail.com>
2020-02-03 20:15:35 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.itheima.git;
+
+/**
+ * 包名：com.itheima.git 文件名：FirstClass
+ *
+ * @author 周鹏飞
+ * @version 1.0
+ * @date 2020/2/3 19:08
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
